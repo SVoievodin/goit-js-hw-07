02bafdec82475c248f765e15712b2c45cbb0b960
Stas Voievodin <vstasv@i.ua>
2023-03-21 13:56:59 +0200

    7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryRef = document.querySelector("ul.gallery");
+
+const imgListRef = galleryItems.map(image => `<li class="gallery__item" "><img  class="gallery__image" src="${image.preview}" alt="${image.description}" </li>`).join('');
+
+galleryRef.insertAdjacentHTML("afterbegin", imgListRef);
+
+const imgOrigListRef = galleryItems.map(image => image.original);
+const imgDescrListRef = galleryItems.map(image => image.description);
+
+galleryRef.addEventListener('click', onClickImage);
+
+function onClickImage(event) {
+    let descrIndex = imgDescrListRef.indexOf(event.target.alt)
+    basicLightbox.create(`
+		<img width="1400" height="900" src="${imgOrigListRef[descrIndex]}">
+	`).show()
+}
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+console.log(instance.show()
+)
 console.log(galleryItems);
